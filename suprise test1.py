d6987838ca2011e023b1110377429cb313eccec4
rm-vrindabhatia <98019825+rm-vrindabhatia@users.noreply.github.com>
2022-01-20 10:24:17 +0530

    Add files via upload
    Funtion for look and say program.

--- suprise test1.py
+++ suprise test1.py
@@ -1,18 +1,17 @@
-def fun_expanding(E):
-    flag=True
-    l=[]
-    for i in range (len(E)):
-        E[i]=int(E[i])
-        a=abs(E[i]-E[i-1])
-        l.append(a)
-    for j in range (len(l)) :
-        if l[i-1]>=l[i]:
-            flag=False
-            break
-    return flag
-E=[]
-L=int(input("Enter length of list"))
-for k in range(L):
-    El=int(input("Enter element"))
-    E.append(El)
-print(fun_expanding(E))
+def Look_and_Say(a):
+    lst = []
+    i = 0
+    while i <len(a):
+        j= 1
+        while i+1<len(a) and a[i]==a[i+1]:
+            i+= 1 
+            j+= 1 
+        lst.append(str(j)+a[i])
+        i+= 1 
+    return ''.join(lst)
+   
+row = int(input())
+num= int(input())
+for i in range (row):
+    print(num)
+    num= Look_and_Say(str(num))
